dd78557413c6d7519db155edefba9825f36a55a2
glqiao971102 <44012470+glqiao971102@users.noreply.github.com>
2020-04-28 13:58:14 +0800

    fixed the search function in order

--- js/main.js
+++ js/main.js
@@ -7,13 +7,16 @@ function myFunction() {
     items = $(".items a")
 
   
-    
+    // // Loop through all list items, and hide those who don't match the search query
     for (let i = 0; i < items.length; i++) {
       txtValue = items.eq(i).attr('data-title')
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        items.eq(i).parent().show();
+        items.eq(i).attr('data-lightbox','images')
         items[i].style.display = "";
       } else {
-        items[i].style.display = "none";
+        items.eq(i).removeAttr('data-lightbox')
+        items.eq(i).parent().hide();
         
       }
     }
